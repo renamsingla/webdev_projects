f00eeb77f80a6701f8cf99fe9dfe14c458af8cb4
renamsingla <renamsingla@gmail.com>
2026-02-24 12:16:02 +0530

    remove unwanted files

--- 6_ChatApp/backend/database/config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-import env from "../../env.js";
-
-export default async () => {
-    return await mongoose.connect(env.DB_URL)
-}
--- 6_ChatApp/backend/prisma/migrations/20251120080232_add_state/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "State" (
-    "id" UUID NOT NULL,
-    "comment" INTEGER NOT NULL,
-
-    CONSTRAINT "State_pkey" PRIMARY KEY ("id")
-);
